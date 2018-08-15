156776980b6a567d4cf4eafba1a8bdc904776bed
Sulaiman <sulaiman@SULAIMAN-iMac.local>
2018-08-15 10:45:55 +0300

    code added

---
+++ calculateAverage.py
@@ -0,0 +1,12 @@
+# get average of three number
+number1 = int(input("Enter number 1: "))
+
+number2 = int(input("Enter number 2: "))
+
+number3 = int(input("Enter number 3: "))
+
+# calculate the average
+average = (number1 + number2 + number3) / 3
+
+# print the average
+print ("the average score is: ", average)
